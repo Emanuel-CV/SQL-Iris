df6058e12d89ab940f1094d189b8bbf1c2696ee8
Emanuel CV <emanuel.coloradov@gmail.com>
2023-05-18 00:24:07 -0500

    iniciando base de datos SQL de instrumentos musicales.

---
+++ ejemplo.sql
@@ -0,0 +1,24 @@
+create database instrumentos;
+show databases;
+use instrumentos;
+
+create table instrumentosMusicales(
+	id int,
+    tipo varchar(50),
+    instrumento varchar(100),
+    precio int,
+    primary key (id)
+);
+
+select * from instrumentosMusicales;
+
+alter table instrumentosMusicales modify column id int auto_increment;
+
+insert into instrumentosMusicales (tipo, instrumento, precio) values
+	('Cuerda', 'Guitarra acústica', 256500),
+    ('Viento', 'Flauta', 65000),
+    ('Cuerda', 'Contrabajo', 1250000);
+    
+select * from instrumentosMusicales;
+
+select * from instrumentosMusicales;
